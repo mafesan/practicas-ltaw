c767c1c1bebd0ebbf67f73236d326665f5a622dd
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2017-05-11 16:43:24 +0200

    Version inicial del servidor nodejs de la tienda. HTML basico de index con mismo estilo que en Django

--- p2-tienda-nodejs/server.js
+++ p2-tienda-nodejs/server.js
@@ -2,44 +2,67 @@
 // Miguel Angel Fernandez Sanchez
 // Para arrancar server: $ nodejs server.js
 
-
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-console.log("Launching NodeJS server...");
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    // BEGINNING OF NEW STUFF
+var mime_types = {
+	'.js' : 'text/javascript',
+	'.html' : 'text/html',
+	'.css' : 'text/css',
+	'.jpg' : 'image/jpg',
+	'.png' : 'image/png',
+	'.jpeg' : 'image/jpeg',
+	'.mp3' : 'audio/mpeg',
+	'.mp4' : 'video/mp4'
+};
 
-    response.on('error', function(err) {
-      console.error(err);
-    });
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
-
-    var responseBody = {
-      headers: headers,
-      method: method,
-      url: url,
-      body: body
-    };
-
-    response.write(JSON.stringify(responseBody));
-    response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
-
-    // END OF NEW STUFF
-  });
+// Creamos el servidor que escucha en el puerto 8080
+var server = http.createServer(function(request, response) {
+    // Mensajes de debug
+    console.log("New request: " + request.url);
+    // Si el pathname es / , se guarda index.html, si no se guardará el pathname que aparezca
+	var pathname = '.' + ((request.url=='/') ? '/index.html' : request.url);
+	var extension = path.extname(pathname);
+   	var contentType = mime_types[extension];
+    console.log(extension);
+    console.log(pathname);
+    console.log(contentType);
+	// Si nos viene un GET, llamamos a la funcion showResponse
+	if (request.method == "GET"){
+		showResponse(pathname, contentType, request, response);
+	}
 }).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
+
+function showResponse(pathname, contentType, request, response){
+	// Comprobamos que la ruta del archivo exista, para ello usamos el modulo fs
+	fs.exists(pathname,function(exist){
+		if(exist){
+			// readFile tiene 2 parámetros: 1- nombre del archivo HTML a leer (indicar todo el path)
+			// y el 2 es una función anónima que tiene dos param.: si hubo error y el contenido del archivo
+			fs.readFile(pathname,function(error,content){
+				if(error){
+					// 500: Internal Server Error
+					response.statusCode = 500;
+					response.setHeader('Content-Type','text/plain');
+					response.write("Internal error");
+					response.end();
+				}else{
+					// 200 OK
+					response.writeHead(200,{'Content-Type': contentType});
+					response.write(content);
+					response.end();
+				}
+			});
+		}else{
+			// 404 not found
+			response.statusCode = 404;
+			response.setHeader('Content-Type','text/plain');
+			response.write('Content not found');
+			response.end();
+		}
+	});
+}
